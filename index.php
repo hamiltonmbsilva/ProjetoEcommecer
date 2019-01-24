87b8bad6ae6142c07f67aea9af1fedd003d77019
Hamilton <hamiltonmb.silva@gmail.com>
2019-01-24 14:03:15 -0200

    Admin/categorias
    Criação de crud de categorias

--- index.php
+++ index.php
@@ -4,10 +4,12 @@ session_start();
 
 require_once("vendor/autoload.php");
 
+
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -216,6 +218,95 @@ $app->post("/admin/forgot/reset", function (){
     ]);
     $page->setTpl("forgot-reset-success");
 });
+
+$app->get("/admin/categories", function (){
+
+    User::verifyLogin();
+
+    $categories = Category::listAll();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories", [
+        'categories'=>$categories
+    ]);
+
+});
+
+$app->get("/admin/categories/create", function (){
+
+    User::verifyLogin();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function (){
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->setData($_POST);
+
+    $category->save();
+
+    header('Location: /admin/categories');
+    exit;
+});
+
+$app->get("/admin/categories/:idcategory/delete", function ($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $category->delete();
+
+    header('Location: /admin/categories');
+    exit;
+
+});
+
+$app->get("/admin/categories/:idcategory", function ($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-update", [
+        'category'=>$category->getValues()
+        ]);
+
+});
+
+$app->post("/admin/categories/:idcategory", function ($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Category();
+
+    $category->get((int)$idcategory);
+
+    $category->setData($_POST);
+
+    $category->save();
+
+    header('Location: /admin/categories');
+    exit;
+
+});
+
+
+
 
 $app->run();
 
