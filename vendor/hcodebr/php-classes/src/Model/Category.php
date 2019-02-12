54efaf89db4b8d8695a000f2bb62d8bee0245b5c
Hamilton <hamiltonmb.silva@gmail.com>
2019-02-12 18:38:26 -0200

    Produtos x Categorias
    Criação de administração de Produtos x categoria e paginação das paginas do produto

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -74,4 +74,86 @@ class Category extends Model{
             implode('', $html));
 
     }
+
+    public function getProducts($related = true){
+
+        $sql = new Sql();
+
+        if($related === true){
+
+                return $sql->select("
+                    SELECT * FROM tb_products WHERE idproduct IN (
+                    SELECT a.idproduct
+                    FROM tb_products a
+                    INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+                    WHERE b.idcategory = :idcategory
+                );
+            
+            ", [
+                ':idcategory'=>$this->getidcategory()
+                ]);
+        }else{
+
+            return $sql->select("
+                    SELECT * FROM tb_products WHERE idproduct NOT IN (
+                    SELECT a.idproduct
+                    FROM tb_products a
+                    INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+                    WHERE b.idcategory = :idcategory
+                );
+            
+            ", [
+                ':idcategory'=>$this->getidcategory()
+            ]);
+
+        }
+    }
+
+    public function getProductsPage($page = 1, $itemsPerPage = 8){
+
+        $start = ($page - 1)* $itemsPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("
+        SELECT sql_calc_found_rows *
+        FROM tb_products a
+        INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+        INNER JOIN tb_categories c On c.idcategory = b.idcategory
+        WHERE c.idcategory = :idcategory
+        limit $start, $itemsPerPage; 
+      ", [
+          ':idcategory'=>$this->getidcategory()
+        ]);
+
+        $resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+        return [
+          'data'=>Product::checkList($results),
+          'total'=>(int)$resultTotal[0]["nrtotal"],
+          'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage)
+        ];
+
+
+    }
+
+    public function addProduct(Product $product){
+
+        $sql = new Sql();
+
+        $sql->query("INSERT INTO tb_productscategories (idcategory, idproduct) VALUES (:idcategory, :idproduct)", [
+            ':idcategory'=>$this->getidcategory(),
+            ':idproduct'=>$product->getidproduct()
+        ]);
+    }
+
+    public function removeProduct(Product $product){
+
+        $sql = new Sql();
+
+        $sql->query("DELETE FROM tb_productscategories WHERE idcategory = :idcategory AND idproduct = :idproduct",[
+            ':idcategory'=>$this->getidcategory(),
+            ':idproduct'=>$product->getidproduct()
+        ]);
+    }
 }
