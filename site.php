f8ff10480dd4e78c8a5cb6a9c2487b19c143346d
Hamilton <hamiltonmb.silva@gmail.com>
2019-02-21 01:37:38 -0300

    Integração pagseguro e integração paypal
    Finalizando as integrações como meio de pagamentos

--- site.php
+++ site.php
@@ -238,6 +238,7 @@ $app->post("/checkout", function(){
 
     $order = new Order();
 
+
     $order->setData([
         'idcart'=>$cart->getidcart(),
         'idaddress'=>$address->getidaddress(),
@@ -246,61 +247,69 @@ $app->post("/checkout", function(){
         'vltotal'=>$cart->getvltotal()
     ]);
 
+
+
     $order->save();
 
-    header("Location: /order/".$order->getidorder());
+    switch ((int)$_POST['payment-method']) {
+        case 1:
+            header("Location: /order".$order->getidorder()."/pagseguro");
+            break;
+        case 2:
+            header("Location: /order".$order->getidorder()."/paypal");
+            break;
+    }
     exit;
-
-//    switch ((int)$_POST['payment-method']) {
-//        case 1:
-//            header("Location: /order/".$order->getidorder()."/pagseguro");
-//            break;
-//        case 2:
-//            header("Location: /order/".$order->getidorder()."/paypal");
-//            break;
-//    }
-//    exit;
 });
 
-//$app->get("/order/:idorder/pagseguro", function($idorder){
-//    User::verifyLogin(false);
-//
-//    $order = new Order();
-//
-//    $order->get((int)$idorder);
-//
-//    $cart = $order->getCart();
-//
-//    $page = new Page([
-//        'header'=>false,
-//        'footer'=>false
-//    ]);
-//
-//    $page->setTpl("payment-pagseguro", [
-//        'order'=>$order->getValues(),
-//        'cart'=>$cart->getValues(),
-//        'products'=>$cart->getProducts(),
-//        'phone'=>[
-//            'areaCode'=>substr($order->getnrphone(), 0, 2),
-//            'number'=>substr($order->getnrphone(), 2, strlen($order->getnrphone()))
-//        ]
-//    ]);
-//});
-//$app->get("/order/:idorder/paypal", function($idorder){
-//    User::verifyLogin(false);
-//    $order = new Order();
-//    $order->get((int)$idorder);
-//    $cart = $order->getCart();
-//    $page = new Page([
-//        'header'=>false,
-//        'footer'=>false
-//    ]);
-//    $page->setTpl("payment-paypal", [
-//        'order'=>$order->getValues(),
-//        'cart'=>$cart->getValues(),
-//        'products'=>$cart->getProducts()
-//    ]);
-//});
+$app->get("/order/:idorder/pagseguro", function($idorder){
+
+    User::verifyLogin(false);
+
+    $order = new Order();
+
+    $order->get((int)$idorder);
+
+    $cart = $order->getCart();
+
+    $page = new Page([
+        'header'=>false,
+        'footer'=>false
+    ]);
+
+
+
+    $page->setTpl("payment-pagseguro", [
+        'order'=>$order->getValues(),
+        'cart'=>$cart->getValues(),
+        'products'=>$cart->getProducts(),
+        'phone'=>[
+            'areaCode'=>substr($order->getnrphone(), 0, 2),
+            'number'=>substr($order->getnrphone(), 2, strlen($order->getnrphone()))
+        ]
+    ]);
+
+});
+
+$app->get("/order/:idorder/paypal", function($idorder){
+    User::verifyLogin(false);
+
+    $order = new Order();
+
+    $order->get((int)$idorder);
+
+    $cart = $order->getCart();
+
+    $page = new Page([
+        'header'=>false,
+        'footer'=>false
+    ]);
+    $page->setTpl("payment-paypal", [
+        'order'=>$order->getValues(),
+        'cart'=>$cart->getValues(),
+        'products'=>$cart->getProducts()
+    ]);
+});
 
 $app->get("/login", function (){
 
--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -19,17 +19,26 @@ class Address extends Model{
     public static function getCEP($nrcep)
     {
         $nrcep = str_replace("-", "", $nrcep);
+
         $ch = curl_init();
+
         curl_setopt($ch, CURLOPT_URL, "http://viacep.com.br/ws/$nrcep/json/");
+
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
         $data = json_decode(curl_exec($ch), true);
+
         curl_close($ch);
+
         return $data;
     }
+
     public function loadFromCEP($nrcep)
     {
         $data = Address::getCEP($nrcep);
+
         if (isset($data['logradouro']) && $data['logradouro']) {
             $this->setdesaddress($data['logradouro']);
             $this->setdescomplement($data['complemento']);
@@ -40,20 +49,24 @@ class Address extends Model{
             $this->setdeszipcode($nrcep);
         }
     }
+
     public function save()
     {
         $sql = new Sql();
-        $results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
+
+        $results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :desnumber, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
             ':idaddress'=>$this->getidaddress(),
             ':idperson'=>$this->getidperson(),
-            ':desaddress'=>utf8_decode($this->getdesaddress()),
-            ':descomplement'=>utf8_decode($this->getdescomplement()),
-            ':descity'=>utf8_decode($this->getdescity()),
-            ':desstate'=>utf8_decode($this->getdesstate()),
-            ':descountry'=>utf8_decode($this->getdescountry()),
+            ':desaddress'=>$this->getdesaddress(),
+            ':desnumber'=>$this->getdesnumber(),
+            ':descomplement'=>$this->getdescomplement(),
+            ':descity'=>$this->getdescity(),
+            ':desstate'=>$this->getdesstate(),
+            ':descountry'=>$this->getdescountry(),
             ':deszipcode'=>$this->getdeszipcode(),
             ':desdistrict'=>$this->getdesdistrict()
         ]);
+
         if (count($results) > 0) {
             $this->setData($results[0]);
         }
